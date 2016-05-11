db5b3a71b6fc95f835011dcc4b816b179bd85105
Veselin Markov <markov@gemtec.eu>
2016-05-11 13:07:14 +0200

    Adds project structure

---
+++ src/org/vaadin/haijian/IColumnValueConverter.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright Gemtec GmbH 2009-2016
+ *
+ * Erstellt am: 11.05.2016 11:11:44
+ * Erstellt von: Veselin Markov
+ */
+package org.vaadin.haijian;
+
+/**
+ * @author Veselin Markov
+ *
+ */
+public interface IColumnValueConverter {
+
+	Object convert(Object toConvert);
+}
